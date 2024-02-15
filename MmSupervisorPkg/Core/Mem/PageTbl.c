5424fe05caa2e442eec3a8ad1d8e337df66b6207
Kun Qin <kuqin@microsoft.com>
2024-02-15 09:24:23 -0800

    Fixing page table

--- MmSupervisorPkg/Core/Mem/PageTbl.c
+++ MmSupervisorPkg/Core/Mem/PageTbl.c
@@ -247,7 +247,9 @@ SmmInitPageTable (
   m1GPageTableSupport           = Is1GPageSupport ();
   m5LevelPagingNeeded           = Is5LevelPagingNeeded ();
   mPhysicalAddressBits          = CalculateMaximumSupportAddress ();
-  PatchInstructionX86 (gPatch5LevelPagingNeeded, m5LevelPagingNeeded, 1);
+  if (SmmCpuFeaturesGetSmiHandlerSize () == 0) {
+    PatchInstructionX86 (gPatch5LevelPagingNeeded, m5LevelPagingNeeded, 1);
+  }
   if (m5LevelPagingNeeded) {
     mPagingMode = m1GPageTableSupport ? Paging5Level1GB : Paging5Level;
   } else {
