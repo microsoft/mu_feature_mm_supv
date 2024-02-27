82018c483408dc72601b58610194241d2962ca24
Kun Qin <kuqin@microsoft.com>
2024-02-27 15:44:23 -0800

    So that we can work around the debug macro error

--- SpamPkg/Core/Runtime/SmmTearDown.c
+++ SpamPkg/Core/Runtime/SmmTearDown.c
@@ -61,7 +61,6 @@ SmmTeardown (
     Rflags = AsmVmResume (&mGuestContextCommonSmm.GuestContextPerCpu[Index].Register);
     // BUGBUG: - AsmVmLaunch if AsmVmResume fail
     if (VmRead32 (VMCS_32_RO_VM_INSTRUCTION_ERROR_INDEX) == VmxFailErrorVmResumeWithNonLaunchedVmcs) {
-//      DEBUG ((EFI_D_ERROR, "(STM):-(\n", (UINTN)Index));
       Rflags = AsmVmLaunch (&mGuestContextCommonSmm.GuestContextPerCpu[Index].Register);
     }
     AcquireSpinLock (&mHostContextCommon.DebugLock);
