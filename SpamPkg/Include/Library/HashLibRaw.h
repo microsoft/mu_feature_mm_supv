4c1c4fad2c6de0becb06aaa0111f551c9a9439e7
kuqin12 <42554914+kuqin12@users.noreply.github.com>
2024-05-24 12:53:26 -0700

    Remove PCR extension from the validation routine (#296)
    # Preface
    
    Please ensure you have read the [contribution
    docs](https://github.com/microsoft/mu/blob/master/CONTRIBUTING.md) prior
    to submitting the pull request. In particular,
    [pull request
    guidelines](https://github.com/microsoft/mu/blob/master/CONTRIBUTING.md#pull-request-best-practices).
    
    ## Description
    
    This change will remove the PCR extension step from the validation
    routine as this will not be picked up by OS anyhow.
    
    However, the routine will keep the hashing functionality on the TPM side
    to avoid including cryptographic functionality in the SPAM core.
    
    For each item, place an "x" in between `[` and `]` if true. Example:
    `[x]`.
    _(you can also check items in the GitHub UI)_
    
    - [x] Impacts functionality?
    - **Functionality** - Does the change ultimately impact how firmware
    functions?
    - Examples: Add a new library, publish a new PPI, update an algorithm,
    ...
    - [x] Impacts security?
    - **Security** - Does the change have a direct security impact on an
    application,
        flow, or firmware?
      - Examples: Crypto algorithm change, buffer overflow fix, parameter
        validation improvement, ...
    - [ ] Breaking change?
    - **Breaking change** - Will anyone consuming this change experience a
    break
        in build or boot behavior?
    - Examples: Add a new library class, move a module to a different repo,
    call
        a function in a new library class in a pre-existing module, ...
    - [ ] Includes tests?
      - **Tests** - Does the change include any explicit test code?
      - Examples: Unit tests, integration tests, robot tests, ...
    - [ ] Includes documentation?
    - **Documentation** - Does the change contain explicit documentation
    additions
        outside direct code modifications (and comments)?
    - Examples: Update readme file, add feature readme file, link to
    documentation
        on an a separate Web page, ...
    
    ## How This Was Tested
    
    This was tested on QEMU Q35 and given test application.
    
    ## Integration Instructions
    
    N/A

---
+++ SpamPkg/Include/Library/HashLibRaw.h
@@ -0,0 +1,51 @@
+/** @file
+  This library abstract TPM2 hash calculation.
+  The platform can choose multiply hash, while caller just need invoke these API.
+  Then all hash value will be returned and/or extended.
+
+Copyright (c) 2013 - 2016, Intel Corporation. All rights reserved. <BR>
+Copyright (c), Microsoft Corporation.
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef HASH_LIB_RAW_H_
+#define HASH_LIB_RAW_H_
+
+/**
+  Hash sequence complete and return the digest list.
+
+  @param HashHandle    Hash handle.
+  @param DataToHash    Data to be hashed.
+  @param DataToHashLen Data size.
+  @param DigestList    Digest list.
+
+  @retval EFI_SUCCESS     Hash sequence complete and DigestList is returned.
+**/
+EFI_STATUS
+EFIAPI
+HashComplete (
+  IN HASH_HANDLE          HashHandle,
+  IN VOID                 *DataToHash,
+  IN UINTN                DataToHashLen,
+  OUT TPML_DIGEST_VALUES  *DigestList
+  );
+
+/**
+  Hash data and return the digest list.
+
+  @param DataToHash    Data to be hashed.
+  @param DataToHashLen Data size.
+  @param DigestList    Digest list.
+
+  @retval EFI_SUCCESS     Hash data and DigestList is returned.
+**/
+EFI_STATUS
+EFIAPI
+HashOnly (
+  IN VOID                 *DataToHash,
+  IN UINTN                DataToHashLen,
+  OUT TPML_DIGEST_VALUES  *DigestList
+  );
+
+#endif
