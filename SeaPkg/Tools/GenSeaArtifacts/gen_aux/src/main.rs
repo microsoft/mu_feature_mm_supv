9cefbc8d43c59297cf72f980b62001c997840c1c
Joey Vagedes <joey.vagedes@gmail.com>
2025-03-28 13:40:10 -0700

    u

--- SeaPkg/Tools/GenSeaArtifacts/gen_aux/src/main.rs
+++ SeaPkg/Tools/GenSeaArtifacts/gen_aux/src/main.rs
@@ -188,7 +188,8 @@ pub fn main() -> Result<()> {
             }
         }
 
-        let report = CoverageReport::build(&efi, &aux)?;
+        let symbols: Vec<Symbol> = parsed_symbols.values().cloned().collect();
+        let report = CoverageReport::build(&efi, &aux, &symbols)?;
 
         report.to_file(output.with_extension("json"))?;
         aux.to_file(output)?;
