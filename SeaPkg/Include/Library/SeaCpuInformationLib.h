0d113b6642f711e574e15f293a84db7f4f8726f3
Michael Kubacki <michael.kubacki@microsoft.com>
2025-05-01 18:31:45 -0400

    Updates

--- SeaPkg/Include/Library/SeaCpuInformationLib.h
+++ SeaPkg/Include/Library/SeaCpuInformationLib.h
@@ -1,5 +1,5 @@
 /** @file
-  Platform CPU Information library header file
+  SEA CPU Information library header file
 
   Copyright (c) Microsoft Corporation.
   SPDX-License-Identifier: BSD-2-Clause-Patent
