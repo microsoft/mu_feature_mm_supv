2e64a750dcc9bf59d514a17c6de3457cb1c39cae
Kun Qin <kuqin@microsoft.com>
2025-06-16 15:52:08 -0700

    vmcall changes

--- SeaPkg/Tests/ResponderValidationTest/ResponderValidationTestApp.c
+++ SeaPkg/Tests/ResponderValidationTest/ResponderValidationTestApp.c
@@ -24,6 +24,8 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Library/UefiLib.h>
 #include <Library/DevicePathLib.h>
 #include <Library/DxeServicesLib.h>
+#include <Library/UefiLib.h>
+#include <Library/StmLib.h>
 
 #include <Protocol/SmmCommunication.h>
 #include <Protocol/MmSupervisorCommunication.h>
@@ -34,11 +36,33 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #define GET_MEMORY_MAP_RETRIES  4
 
-VOID   *mPiSmmCommonCommBufferAddress = NULL;
-UINTN  mPiSmmCommonCommBufferSize;
-
 STATIC EFI_EVENT  mExitBootServicesEvent       = NULL;
 EFI_HANDLE        gTestImageHandle = NULL;
+VOID              *TestVmxOnBuffer = NULL;
+VOID              *TestVmcsBuffer = NULL;
+VOID              *TestCommBuffer = NULL;
+
+/**
+
+  This function return 4K page aligned VMCS size.
+
+  @return 4K page aligned VMCS size
+
+**/
+UINT32
+GetVmcsSize (
+  VOID
+  )
+{
+  UINT64  Data64;
+  UINT32  VmcsSize;
+
+  Data64   = AsmReadMsr64 (IA32_VMX_BASIC_MSR_INDEX);
+  VmcsSize = (UINT32)(RShiftU64 (Data64, 32) & 0xFFFF);
+  VmcsSize = STM_PAGES_TO_SIZE (STM_SIZE_TO_PAGES (VmcsSize));
+
+  return VmcsSize;
+}
 
 /**
   This helper function actually sends the requested communication
@@ -51,230 +75,86 @@ EFI_HANDLE        gTestImageHandle = NULL;
 **/
 STATIC
 EFI_STATUS
-DxeToSmmCommunicate (
+InvokeVmcalls (
   IN EFI_EVENT  Event,
   IN VOID       *Context
   )
 {
-  EFI_STATUS                            Status            = EFI_SUCCESS;
-  MM_SUPERVISOR_COMMUNICATION_PROTOCOL  *SmmCommunication = NULL;
-  VOID                                  *CommBufferBase;
-  EFI_SMM_COMMUNICATE_HEADER            *CommHeader;
-  UINTN                                 MinBufferSize, BufferSize;
-  SEA_TEST_COMM_INPUT_REGION            *SeaTestCommInputBuffer;
-  SEA_TEST_COMM_OUTPUT_REGION           *SeaTestCommOutputputBuffer;
+  IA32_CR4 Cr4;
+  UINT64  u64;
+  VM_EXIT_CONTROLS    VmExitCtrls;
+  VM_ENTRY_CONTROLS   VmEntryCtrls;
+  IA32_VMX_BASIC_MSR  VmxBasicMsr;
 
-  DEBUG ((DEBUG_INFO, "%a()\n", __func__));
+  Print (L"%a - Enable VMX...\n", __func__);
+  Cr4.UintN = AsmReadCr4 ();
+  Cr4.Bits.VMXE = TRUE;
+  AsmWriteCr4 (Cr4.UintN);
+  Print (L"%a - VMX enabled!\n", __func__);
 
-  //
-  // Make sure that we have access to a buffer that seems to be sufficient to do everything we need to do.
-  //
-  if (mPiSmmCommonCommBufferAddress == NULL) {
-    DEBUG ((DEBUG_ERROR, "%a - Communication mBuffer not found!\n", __func__));
-    return EFI_ABORTED;
+  VmxBasicMsr.Uint64 = AsmReadMsr64 (IA32_VMX_BASIC_MSR_INDEX);
+  Print (L"%a - IA32_VMX_BASIC_MSR = 0x%lx\n", __func__, VmxBasicMsr.Uint64);
+
+  Print (L"%a - Enter VMX root mode...\n", __func__);
+  AsmVmxOn (TestVmcsBuffer);
+  Print (L"%a - VMX root mode entered!\n", __func__);
+
+  Print (L"Clear VMCS buffer...\n");
+  AsmVmClear (TestVmcsBuffer);
+  Print (L"%a - Vmcs buffer cleared!\n", __func__);
+
+  Print (L"Load VMCS buffer for this core...\n");
+  AsmVmPtrLoad (TestVmcsBuffer);
+  Print (L"%a - VMCS buffer loaded!\n", __func__);
+
+  u64 = AsmReadMsr64 (IA32_VMX_EXIT_CTLS_MSR_INDEX);
+  VmExitCtrls.Uint32 = (UINT32)u64 & (UINT32)RShiftU64 (u64, 32);
+  Print (L"%a - VmxExitCtrls = 0x%x\n", __func__, VmExitCtrls.Uint32);
+
+  VmExitCtrls.Bits.SaveIA32_EFER = TRUE;
+  VmExitCtrls.Bits.Ia32eHost = TRUE;
+  VmWrite32 (VMCS_32_CONTROL_VMEXIT_CONTROLS_INDEX, VmExitCtrls.Uint32);
+
+  u64 = AsmReadMsr64 (IA32_VMX_ENTRY_CTLS_MSR_INDEX);
+  VmEntryCtrls.Uint32 = (UINT32)u64 & (UINT32)RShiftU64 (u64, 32);
+  Print (L"%a - VmxEntryCtrls = 0x%lx\n", __func__, VmxEntryCtrls.Uint32);
+
+  VmEntryCtrls.Bits.LoadIA32_EFER = TRUE;
+  VmEntryCtrls.Bits.Ia32eGuest = TRUE;
+  VmWrite32 (VMCS_32_CONTROL_VMENTRY_CONTROLS_INDEX, VmEntryCtrls.Uint32);
+
+  Print (L"%a - VMCS controls set!\n", __func__);
+
+  // Ready to get capabilities from SEA through VMCALL
+  Print (L"%a - Getting capabilities from SEA...\n", __func__);
+  Ret = AsmVmCall (SEA_API_GET_CAPABILITIES, (UINT32)(UINTN)TestCommBuffer, 0, 0);
+  Print (L"%a - Getting capabilities completed - 0x%x!\n", __func__, Ret);
+
+  if (Ret != STM_SUCCESS) {
+    Print (L"ERROR - AsmVmCall returned %r\n", Ret);
+    return EFI_DEVICE_ERROR;
   }
 
-  MinBufferSize = OFFSET_OF (EFI_SMM_COMMUNICATE_HEADER, Data) + sizeof (SEA_TEST_COMM_INPUT_REGION) + PcdGetSize (PcdAuxBinFile);
-  if (MinBufferSize > mPiSmmCommonCommBufferSize) {
-    DEBUG ((DEBUG_ERROR, "%a - Communication mBuffer is too small\n", __func__));
-    return EFI_BUFFER_TOO_SMALL;
+  Print (L"%a - Getting resources from SEA...\n", __func__);
+  Ret = AsmVmCall (SEA_API_GET_RESOURCES, 0, 0, 0);
+  Print (L"%a - Getting resources completed - 0x%x!\n", __func__, Ret);
+
+  if (Ret != ERROR_STM_BUFFER_TOO_SMALL) {
+    Print (L"ERROR - AsmVmCall returned %r\n", Ret);
+    return EFI_DEVICE_ERROR;
   }
 
-  CommBufferBase = mPiSmmCommonCommBufferAddress;
+  Print (L"%a - Getting resources with buffer size %d...\n", __func__, EFI_PAGE_SIZE);
+  Ret = AsmVmCall (SEA_API_GET_RESOURCES, (UINT32)(UINTN)TestCommBuffer, 1, 0);
+  Print (L"%a - BSP getting resources completed - 0x%x!\n", __func__, Ret);
 
-  //
-  // Locate the protocol as needed.
-  //
-  if (SmmCommunication == NULL) {
-    Status = gBS->LocateProtocol (&gMmSupervisorCommunicationProtocolGuid, NULL, (VOID **)&SmmCommunication);
-    if (EFI_ERROR (Status)) {
-      return Status;
-    }
+  if (Ret != STM_SUCCESS) {
+    Print (L"ERROR - AsmVmCall returned %r\n", Ret);
+    return EFI_DEVICE_ERROR;
   }
 
-  //
-  // Prep the buffer for getting the last of the misc data.
-  //
-  ZeroMem (CommBufferBase, mPiSmmCommonCommBufferSize);
-  CommHeader = CommBufferBase;
-  CopyGuid (&CommHeader->HeaderGuid, &gSeaValidationTestHandlerGuid);
-  CommHeader->MessageLength = MinBufferSize - OFFSET_OF (EFI_SMM_COMMUNICATE_HEADER, Data);
-
-  SeaTestCommInputBuffer                        = (SEA_TEST_COMM_INPUT_REGION *)((UINTN)CommHeader + OFFSET_OF (EFI_SMM_COMMUNICATE_HEADER, Data));
-  SeaTestCommInputBuffer->SupervisorAuxFileSize = PcdGetSize (PcdAuxBinFile);
-  CopyMem (SeaTestCommInputBuffer->SupervisorAuxFileBase, PcdGetPtr (PcdAuxBinFile), PcdGetSize (PcdAuxBinFile));
-
-  SeaTestCommInputBuffer->SupvDigestList[MMI_ENTRY_DIGEST_INDEX].digests[0].hashAlg = TPM_ALG_SHA256;
-  SeaTestCommInputBuffer->SupvDigestList[MMI_ENTRY_DIGEST_INDEX].count              = 1;
-  CopyMem (SeaTestCommInputBuffer->SupvDigestList[MMI_ENTRY_DIGEST_INDEX].digests[0].digest.sha256, PcdGetPtr (PcdMmiEntryBinHash), SHA256_DIGEST_SIZE);
-
-  SeaTestCommInputBuffer->SupvDigestList[MM_SUPV_DIGEST_INDEX].digests[0].hashAlg = TPM_ALG_SHA256;
-  SeaTestCommInputBuffer->SupvDigestList[MM_SUPV_DIGEST_INDEX].count              = 1;
-  CopyMem (SeaTestCommInputBuffer->SupvDigestList[MM_SUPV_DIGEST_INDEX].digests[0].digest.sha256, PcdGetPtr (PcdMmSupervisorCoreHash), SHA256_DIGEST_SIZE);
-
-  SeaTestCommInputBuffer->SupvDigestListCount = SUPPORTED_DIGEST_COUNT;
-  SeaTestCommInputBuffer->MmiEntryFileSize    = PcdGet64 (PcdMmiEntryBinSize);
-
-  BufferSize = mPiSmmCommonCommBufferSize;
-
-  //
-  // Signal trip to SMM.
-  //
-  Status = SmmCommunication->Communicate (
-                               SmmCommunication,
-                               CommBufferBase,
-                               &BufferSize
-                               );
-  if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_ERROR, "%a - Communication failed - %r\n", __func__, Status));
-    goto Exit;
-  }
-
-  SeaTestCommOutputputBuffer = (SEA_TEST_COMM_OUTPUT_REGION *)(CommHeader + 1);
-  DEBUG ((DEBUG_INFO, "%a - FirmwarePolicy: %p\n", __func__, &SeaTestCommOutputputBuffer->FirmwarePolicy));
-
-Exit:
-  return Status;
-} // DxeToSmmCommunicate()
-
-/**
- * @brief      Locates and stores address of comm buffer.
- *
- * @return     EFI_ABORTED if buffer has already been located, error
- *             from getting system table, or success.
- */
-EFI_STATUS
-EFIAPI
-LocateSmmCommonCommBuffer (
-  VOID
-  )
-{
-  EDKII_PI_SMM_COMMUNICATION_REGION_TABLE  *PiSmmCommunicationRegionTable;
-  EFI_MEMORY_DESCRIPTOR                    *SmmCommMemRegion;
-  UINTN                                    Index, BufferSize;
-  EFI_STATUS                               Status = EFI_ABORTED;
-  UINTN                                    DesiredBufferSize;
-
-  if (mPiSmmCommonCommBufferAddress == NULL) {
-    Status = EfiGetSystemConfigurationTable (&gMmSupervisorCommunicationRegionTableGuid, (VOID **)&PiSmmCommunicationRegionTable);
-    if (EFI_ERROR (Status)) {
-      DEBUG ((DEBUG_ERROR, "%a Failed to get system configuration table %r\n", __func__, Status));
-      return Status;
-    }
-
-    Status = EFI_BAD_BUFFER_SIZE;
-
-    DesiredBufferSize = sizeof (EFI_SMM_COMMUNICATE_HEADER);
-    DEBUG ((DEBUG_ERROR, "%a desired comm buffer size %ld\n", __func__, DesiredBufferSize));
-    BufferSize       = 0;
-    SmmCommMemRegion = (EFI_MEMORY_DESCRIPTOR *)(PiSmmCommunicationRegionTable + 1);
-    for (Index = 0; Index < PiSmmCommunicationRegionTable->NumberOfEntries; Index++) {
-      if (SmmCommMemRegion->Type == EfiConventionalMemory) {
-        BufferSize = EFI_PAGES_TO_SIZE ((UINTN)SmmCommMemRegion->NumberOfPages);
-        if (BufferSize >= (DesiredBufferSize + OFFSET_OF (EFI_SMM_COMMUNICATE_HEADER, Data))) {
-          Status = EFI_SUCCESS;
-          break;
-        }
-      }
-
-      SmmCommMemRegion = (EFI_MEMORY_DESCRIPTOR *)((UINT8 *)SmmCommMemRegion + PiSmmCommunicationRegionTable->DescriptorSize);
-    }
-
-    mPiSmmCommonCommBufferAddress = (VOID *)SmmCommMemRegion->PhysicalStart;
-    mPiSmmCommonCommBufferSize    = BufferSize;
-  }
-
-  return Status;
-} // LocateSmmCommonCommBuffer()
-
-/**
-  Measure PE image into TPM log based on the authenticode image hashing in
-  PE/COFF Specification 8.0 Appendix A.
-
-  Caution: This function may receive untrusted input.
-  PE/COFF image is external input, so this function will validate its data structure
-  within this image buffer before use.
-
-  @param[in] MeasureBootProtocols   Pointer to the located MeasureBoot protocol instances.
-  @param[in] ImageAddress           Start address of image buffer.
-  @param[in] ImageSize              Image size
-  @param[in] LinkTimeBase           Address that the image is loaded into memory.
-  @param[in] ImageType              Image subsystem type.
-  @param[in] FilePath               File path is corresponding to the input image.
-
-  @retval EFI_SUCCESS            Successfully measure image.
-  @retval EFI_OUT_OF_RESOURCES   No enough resource to measure image.
-  @retval EFI_UNSUPPORTED        ImageType is unsupported or PE image is mal-format.
-  @retval other error value
-**/
-EFI_STATUS
-EFIAPI
-Tcg2MeasurePeImage (
-  IN  EFI_PHYSICAL_ADDRESS  ImageAddress,
-  IN  UINTN                 ImageSize
-  )
-{
-  EFI_STATUS         Status;
-  EFI_TCG2_EVENT     *Tcg2Event;
-  UINT32             EventSize;
-  EFI_TCG2_PROTOCOL  *Tcg2Protocol;
-  UINT8              *EventPtr;
-
-  Status    = EFI_UNSUPPORTED;
-  EventPtr  = NULL;
-  Tcg2Event = NULL;
-
-  Status = gBS->LocateProtocol (&gEfiTcg2ProtocolGuid, NULL, (VOID **)&Tcg2Protocol);
-  if (EFI_ERROR (Status) || (Tcg2Protocol == NULL)) {
-    ASSERT (FALSE);
-    return EFI_UNSUPPORTED;
-  }
-
-  EventSize = OFFSET_OF (EFI_TCG2_EVENT, Event);
-
-  //
-  // Determine destination PCR by BootPolicy
-  //
-  // from a malicious GPT disk partition
-  EventPtr = AllocateZeroPool (EventSize);
-  if (EventPtr == NULL) {
-    return EFI_OUT_OF_RESOURCES;
-  }
-
-  Tcg2Event                       = (EFI_TCG2_EVENT *)EventPtr;
-  Tcg2Event->Size                 = EventSize;
-  Tcg2Event->Header.HeaderSize    = sizeof (EFI_TCG2_EVENT_HEADER);
-  Tcg2Event->Header.HeaderVersion = EFI_TCG2_EVENT_HEADER_VERSION;
-  Tcg2Event->Header.EventType     = EV_EFI_BOOT_SERVICES_APPLICATION;
-  Tcg2Event->Header.PCRIndex      = 0;
-
-  //
-  // Log the PE data
-  //
-  Status = Tcg2Protocol->HashLogExtendEvent (
-                           Tcg2Protocol,
-                           PE_COFF_IMAGE,
-                           ImageAddress,
-                           ImageSize,
-                           Tcg2Event
-                           );
-  DEBUG ((DEBUG_INFO, "DxeTpm2MeasureBootHandler - Tcg2 MeasurePeImage - %r\n", Status));
-
-  if (Status == EFI_VOLUME_FULL) {
-    //
-    // Volume full here means the image is hashed and its result is extended to PCR.
-    // But the event log can't be saved since log area is full.
-    // Just return EFI_SUCCESS in order not to block the image load.
-    //
-    Status = EFI_SUCCESS;
-  }
-
-  if (EventPtr != NULL) {
-    FreePool (EventPtr);
-  }
-
-  return Status;
-}
+  return EFI_SUCCESS;
+} // InvokeVmcalls()
 
 /**
   ResponderValidationTestAppEntry
@@ -293,12 +173,7 @@ ResponderValidationTestAppEntry (
   IN     EFI_SYSTEM_TABLE  *SystemTable
   )
 {
-  DEBUG ((DEBUG_INFO, "%a the app's up!\n", __func__));
-
-  if (EFI_ERROR (LocateSmmCommonCommBuffer ())) {
-    DEBUG ((DEBUG_ERROR, "%a Comm buffer setup failed\n", __func__));
-    return EFI_ABORTED;
-  }
+  Print (L"%a the app's up!\n", __func__);
 
   //
   // Get the EFI memory map.
@@ -311,11 +186,33 @@ ResponderValidationTestAppEntry (
   UINTN                  EfiDescriptorSize;
   UINT32                 EfiDescriptorVersion;
 
+  TestVmxOnBuffer = AllocateAlignedPages (EFI_SIZE_TO_PAGES (GetVmcsSize ()), EFI_PAGE_SIZE);
+  if (TestVmxOnBuffer == NULL) {
+    Print (L"ERROR - Failed to allocate VMXON buffer!\n");
+    FreePool (TestVmcsBuffer);
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  TestVmcsBuffer = AllocateAlignedPages (EFI_SIZE_TO_PAGES (GetVmcsSize ()), EFI_PAGE_SIZE);
+  if (TestVmcsBuffer == NULL) {
+    Print (L"ERROR - Failed to allocate VMCS buffer!\n");
+    FreePool (TestVmxOnBuffer);
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  TestCommBuffer = AllocatePages (1);
+  if (TestCommBuffer == NULL) {
+    Print (L"ERROR - Failed to allocate communication buffer!\n");
+    FreePool (TestVmcsBuffer);
+    FreePool (TestVmxOnBuffer);
+    return EFI_OUT_OF_RESOURCES;
+  }
+
   // Install EBS callback handler
   Status = gBS->CreateEventEx (
         EVT_NOTIFY_SIGNAL,
         (TPL_APPLICATION + 1),
-        DxeToSmmCommunicate,
+        InvokeVmcalls,
         gImageHandle,
         &gEfiEventExitBootServicesGuid,
         &mExitBootServicesEvent
@@ -336,7 +233,7 @@ ResponderValidationTestAppEntry (
                               &EfiDescriptorVersion
                               );
     if ((Status != EFI_BUFFER_TOO_SMALL) || !EfiMemoryMapSize) {
-      DEBUG ((DEBUG_ERROR, "GetMemoryMap Error %r\n", Status));
+      Print (L"GetMemoryMap Error %r\n", Status);
       return EFI_BAD_BUFFER_SIZE;
     }
 
@@ -356,7 +253,7 @@ ResponderValidationTestAppEntry (
     //
     // Create exit boot services event
     //
-    DEBUG ((DEBUG_INFO, "Calling ExitBootServices - Retry = %d\n", Retry));
+    Print (L"Calling ExitBootServices - Retry = %d\n", Retry);
     Status = gBS->ExitBootServices (
                     gTestImageHandle,
                     EfiMapKey
@@ -364,11 +261,11 @@ ResponderValidationTestAppEntry (
   } while (EFI_ERROR (Status) && Retry++ < GET_MEMORY_MAP_RETRIES);
 
   if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_ERROR, "ERROR - Exit Boot Services returned %r\n", Status));
+    Print (L"ERROR - Exit Boot Services returned %r\n", Status);
   }
 
   // Should not be here!!!
-  DEBUG ((DEBUG_INFO, "%a the app's done!\n", __func__));
+  Print (L"%a the app's done!\n", __func__);
 
   return EFI_SUCCESS;
 } // ResponderValidationTestAppEntry()
