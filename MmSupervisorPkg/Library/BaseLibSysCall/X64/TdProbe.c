fc8890542bef3ef379f97b5e35bcea1073e1d767
Kun Qin <kuqin@microsoft.com>
2025-10-02 16:25:26 -0700

    adding td calls

---
+++ MmSupervisorPkg/Library/BaseLibSysCall/X64/TdProbe.c
@@ -0,0 +1,25 @@
+/** @file
+
+  Copyright (c) 2020-2021, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Library/BaseLib.h>
+#include <Register/Intel/Cpuid.h>
+
+/**
+  Probe if TD is enabled.
+
+  @return TRUE    TD is enabled.
+  @return FALSE   TD is not enabled.
+**/
+BOOLEAN
+EFIAPI
+TdIsEnabled (
+  )
+{
+  // We should not even invoke this function for supervised environment.
+  ASSERT (FALSE);
+  return FALSE;
+}
