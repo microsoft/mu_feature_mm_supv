a0634dbab9c57298e25790b2bfbbd79484de6d6d
Kun Qin <kuqin@microsoft.com>
2025-12-18 14:38:56 -0800

    add back a dummy invd instruction so that we can still make it work

---
+++ MmSupervisorPkg/Library/BaseLibSysCall/X64/Invd.c
@@ -0,0 +1,33 @@
+/** @file
+  CpuBreakpoint function.
+
+  Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
+  Copyright (C), Microsoft Corporation.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+/**
+  Microsoft Visual Studio 7.1 Function Prototypes for I/O Intrinsics.
+**/
+
+#include    <Uefi.h>
+#include    <Library/BaseLib.h>
+#include    <Library/SysCallLib.h>
+
+/**
+  Executes a INVD instruction.
+
+  Executes a INVD instruction. This function is only available on IA-32 and
+  x64.
+
+**/
+VOID
+EFIAPI
+AsmInvd (
+  VOID
+  )
+{
+  // We should not even invoke this function for supervised environment.
+  ASSERT (FALSE);
+}
