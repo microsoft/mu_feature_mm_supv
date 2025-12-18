dce2c5c81fcc30675e73b8124315471a6756296e
kuqin12 <42554914+kuqin12@users.noreply.github.com>
2025-12-18 15:42:10 -0800

    Add a dummy `AsmInvd` function to suffice the usage from secondary libraries (#595)
    ## Description
    
    The `CacheMaintenaceLib` is implementing its own functions based on a
    few `BaseLib` primitives.
    
    Thus for any supervised standalone MM module that links cache
    maintenance library, the build could break.
    
    This change bring back the function symbol but do not provide any
    implementation to resolve the build break but does not change the
    functionality of the original code.
    
    - [ ] Impacts functionality?
    - [x] Impacts security?
    - [ ] Breaking change?
    - [ ] Includes tests?
    - [ ] Includes documentation?
    
    ## How This Was Tested
    
    This was tested on physical hardware platform and fixed the build.
    
    ## Integration Instructions
    
    N/A
    
    ---------
    
    Signed-off-by: kuqin12 <42554914+kuqin12@users.noreply.github.com>
    Co-authored-by: Michael Kubacki <michael.kubacki@microsoft.com>

---
+++ MmSupervisorPkg/Library/BaseLibSysCall/X64/Invd.c
@@ -0,0 +1,29 @@
+/** @file
+  CpuBreakpoint function.
+
+  Copyright (c) 2006 - 2010, Intel Corporation. All rights reserved.<BR>
+  Copyright (C), Microsoft Corporation.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include    <Uefi.h>
+#include    <Library/BaseLib.h>
+#include    <Library/DebugLib.h>
+
+/**
+  Executes a INVD instruction.
+
+  Executes a INVD instruction. This function is only available on IA-32 and
+  x64.
+
+**/
+VOID
+EFIAPI
+AsmInvd (
+  VOID
+  )
+{
+  // We should not even invoke this function for supervised environment.
+  ASSERT (FALSE);
+}
